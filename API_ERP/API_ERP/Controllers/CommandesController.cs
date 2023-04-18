d551a1d9882d459f81c362ba6b172bd3e1dbae94
Mael <78413891+Urban-79@users.noreply.github.com>
2023-04-18 17:10:43 +0200

    Ajout Controlleurs
    controlleur : command + products

---
+++ API_ERP/API_ERP/Controllers/CommandesController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.ComponentModel.DataAnnotations;
+using API_ERP.Class;
+
+namespace API_ERP.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CommandesController : ControllerBase
+    {
+        /// <summary>
+        /// Get Commande
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns>test</returns>
+
+        [HttpGet]
+        public async Task<IActionResult> GetCommand(string id)
+        {
+            if (string.IsNullOrEmpty(id))
+            {
+                return BadRequest();
+            }
+
+            using (var httpClient = new HttpClient())
+            {
+                var apiUrl = "https://615f5fb4f7254d0017068109.mockapi.io/api/v1/command/" + id;
+                var response = await httpClient.GetAsync(apiUrl);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var json = await response.Content.ReadAsStringAsync();
+                    var result = JsonConvert.DeserializeObject<Command>(json);
+                    return Ok(result);
+                }
+
+                return BadRequest();
+            }
+        }
+
+    }
+}
---
+++ API_ERP/API_ERP/Controllers/productsController.cs
@@ -0,0 +1,42 @@
+﻿using API_ERP.Class;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace API_ERP.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class productsController : ControllerBase
+    {
+        /// <summary>
+        /// Get Commande
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns>test</returns>
+
+        [HttpGet]
+        public async Task<IActionResult> GetProduct(string id)
+        {
+            if (string.IsNullOrEmpty(id))
+            {
+                return BadRequest();
+            }
+
+            using (var httpClient = new HttpClient())
+            {
+                var apiUrl = "https://615f5fb4f7254d0017068109.mockapi.io/api/v1/products/" + id;
+                var response = await httpClient.GetAsync(apiUrl);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var json = await response.Content.ReadAsStringAsync();
+                    var result = JsonConvert.DeserializeObject<Command>(json);
+                    return Ok(result);
+                }
+
+                return BadRequest();
+            }
+        }
+    }
+}
