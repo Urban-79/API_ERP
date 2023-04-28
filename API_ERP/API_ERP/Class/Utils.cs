7c32f54cedc77d42b74b5d81895141a5771e19ca
Mael <78413891+Urban-79@users.noreply.github.com>
2023-04-28 09:20:33 +0200

    Merge

---
+++ API_ERP/API_ERP/Class/Utils.cs
@@ -0,0 +1,60 @@
+﻿using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Net.Mail;
+using QRCoder;
+
+namespace API_ERP.Class
+{
+    public static class Utils
+    {
+        /// <summary>
+        /// Générer un QRCode
+        /// </summary>
+        /// <param name="data"></param>
+        /// <param name="fileName"></param>
+        public static String GenerateQRCode(string data, string fileName)
+        {
+            QRCodeGenerator qrGenerator = new QRCodeGenerator();
+            QRCodeData qrCodeData = qrGenerator.CreateQrCode(data, QRCodeGenerator.ECCLevel.Q);
+            QRCode qrCode = new QRCode(qrCodeData);
+            Bitmap qrCodeImage = qrCode.GetGraphic(20);
+
+            qrCodeImage.Save(fileName, ImageFormat.Png);
+            return fileName;
+        }
+
+        /// <summary>
+        /// Envoi d'un email
+        /// </summary>
+        /// <param name="fromAddress"></param>
+        /// <param name="toAddress"></param>
+        /// <param name="subject"></param>
+        /// <param name="body"></param>
+        /// <param name="fileName"></param>
+        /// <param name="password"></param>
+        public static void SendEmailWithAttachment(string fromAddress, string toAddress, string subject, string body,
+            string fileName, string password)
+        {
+            MailMessage message = new MailMessage();
+            message.From = new MailAddress(fromAddress);
+            message.To.Add(toAddress);
+            message.Subject = subject;
+            message.Body = body;
+
+            // Ajouter la pièce jointe
+            Attachment attachment = new Attachment(fileName);
+            message.Attachments.Add(attachment);
+
+            SmtpClient
+                smtp = new SmtpClient("smtp.example.com",
+                    587); // Remplacez "smtp.example.com" par le nom de votre serveur SMTP
+            smtp.UseDefaultCredentials = false;
+            smtp.Credentials =
+                new System.Net.NetworkCredential(fromAddress,
+                    password); // Remplacez "votre_mot_de_passe" par votre propre mot de passe
+            smtp.EnableSsl = true;
+            smtp.Send(message);
+        }
+    }
+}
---
+++ API_ERP/API_ERP/Tests/productsTest.cs
@@ -0,0 +1,68 @@
+﻿using System.Threading.Tasks;
+using API_ERP.Controllers;
+using API_ERP.Class;
+using Microsoft.AspNetCore.Mvc;
+using Xunit;
+
+namespace API_ERP.Tests
+{
+    public class ProductsControllerTests
+    {
+        private readonly ProductsController _controller;
+        
+        public ProductsControllerTests()
+        {
+            var service = new ProductApiService(); // Remplacer par votre service de test
+            _controller = new ProductsController(service);
+        }
+
+        [Fact]
+        public async Task GetProduct_ReturnsBadRequest_WhenIdIsNull()
+        {
+            // Arrange
+            string id = null;
+
+            // Act
+            var result = await _controller.GetProduct(id);
+
+            // Assert
+            var badRequestResult = Assert.IsType<BadRequestResult>(result);
+            Assert.Equal(400, badRequestResult.StatusCode);
+        }
+
+        [Fact]
+        public async Task GetProduct_ReturnsNotFound_WhenProductDoesNotExist()
+        {
+            // Arrange
+            string id = "non-existing-id";
+
+            // Act
+            var result = await _controller.GetProduct(id);
+
+            // Assert
+            var notFoundResult = Assert.IsType<NotFoundResult>(result);
+            Assert.Equal(404, notFoundResult.StatusCode);
+        }
+
+        // [Fact]
+        // public async Task GetProduct_ReturnsOk_WhenProductExists()
+        // {
+        //     // Arrange
+        //     string id = "existing-id";
+        //     var mockService = new Mock<ProductApiService>();
+        //     mockService.Setup(s => s.GetProductAsync(id))
+        //         .ReturnsAsync(new Product { Id = id });
+        //     var controller = new ProductsController(mockService.Object);
+        //
+        //     // Act
+        //     var result = await controller.GetProduct(id);
+        //
+        //     // Assert
+        //     var okResult = Assert.IsType<OkObjectResult>(result);
+        //     var product = Assert.IsType<Product>(okResult.Value);
+        //     Assert.Equal(id, product.Id);
+        //     mockService.Verify(s => s.GetProductAsync(id), Times.Once);
+        // }
+    }
+
+}
